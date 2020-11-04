76fefd5697bc149e5b74b0e8707d05c695e070a4
Jeffery <blakely4206@gmail.com>
2020-11-03 18:31:24 -0600

    Cleaned code; added comments

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, make_response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_socketio import SocketIO, emit
@@ -14,13 +14,8 @@ if not os.getenv("DATABASE_URL"):
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
-logged_in = False
-
-current_room = "Main"
 
 chat_log = []
-chat_log1 = []
-chat_log2 = []
 
 chat_rooms = {}
 
@@ -29,61 +24,74 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/", methods=["GET", "POST"])
-def login():    
-    if request.method == "GET":        
+def login():  
+    #GET  
+    if request.method == "GET":       
+        #if there is a user_id in session 
         if session.get("user_id") != None:
+            #if the user has a channel in session
             if session.get("current_channel") != None:
-                url = 'channel/' + session["current_channel"]
+                #open that channel
                 return redirect(url_for('channel_view', room=session["current_channel"]))
             else:
+                #else main page
                 return redirect(url_for('index'))
         else:
+            #send user to login page
             return render_template("login.html")
+    #POST
     else:
-        session["user_id"] = request.form.get("user_id")
-        name = session.get("user_id")
+        #search database for user
+        name = request.form.get("user_id")        
         rows = db.execute("SELECT * FROM users where name = :name;", {"name": name})
         result = rows.fetchone()
+        #User not found, send to create user page
         if(result == None):
-            print("YES!")
-            return redirect("/create")
+            return redirect("/register")
+        #user found, send to index page, set session user data
         else:
-            print("NO!")
+            session["user_id"] = name
             return redirect("/index")
-        
+
 @app.route("/index", methods=["GET", "POST"])
 def index():
+    #if user is logged in
     if session.get("user_id") != None:
-        # todo select all current chat rooms        
-     #   rooms = db.execute("SELECT name FROM chatroom;")
-    #    for name in rooms:
-    #        name = mame.replace("(", "").replace(")", "").replace("'", "")
-            
         return render_template("index.html", user_id = session["user_id"], chat_log = chat_log, chat_rooms = chat_rooms)
+    #user is not logged in redirect 
     else:
         return redirect("/")
 
-@app.route("/register")
+#register new user
+@app.route("/register",  methods=["GET","POST"])
 def register():
-    return render_template("register.html")
-
-@app.route("/create", methods=["POST"])
-def create():
-    user_name = request.form.get("user_name")
-    user_id = request.form.get("user_id")
-    password = request.form.get("password")
+    #GET
+    if request.method == "GET":
+        return render_template("register.html")
+    #POST (Create a user)
+    else:
+        #TODO VALIDATE USER INPUT
+        user_name = request.form.get("user_name")
+        user_id = request.form.get("user_id")
+        password = request.form.get("password")
     
-    db.execute("INSERT INTO users values (:user_id, :user_name, :password)", {"user_id": user_id, "user_name": user_name, "password": password})
-    db.execute("COMMIT")
+        #insert into database
+        db.execute("INSERT INTO users values (:user_id, :user_name, :password)", {"user_id": user_id, "user_name": user_name, "password": password})
+        db.execute("COMMIT")
     
-    return render_template("index.html")
+        #return index page
+        session["user_id"] = user_id
+        return render_template("index.html")
     
 @app.route("/channel/<string:room>", methods=["GET", "POST"])
 def channel_view(room):
-    session["current_channel"] = room
-    this_chat_log = chat_rooms[room]
-    return render_template("channel.html", user_id = session["user_id"], room=room, chat_log = this_chat_log, chat_rooms = chat_rooms)
-  
+    if session.get("user_id") != None:
+        session["current_channel"] = room
+        this_chat_log = chat_rooms[room]
+        return render_template("channel.html", user_id = session["user_id"], room=room, chat_log = this_chat_log, chat_rooms = chat_rooms)
+    else:
+        redirect("/")
+
 @app.route("/create_channel", methods=["GET", "POST"])
 def create_channel():
     if request.method == "GET":
